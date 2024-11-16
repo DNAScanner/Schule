69500dc8b103d12ef7e5c7c64807303fe8294693
DNAScanner <main@dnascanner.de>
2024-11-17 00:51:11 +0100

    d2-Test funktionsfähig

--- K10/NwT/Projekt 1/Tests/Website/index.js
+++ K10/NwT/Projekt 1/Tests/Website/index.js
@@ -1,14 +1,18 @@
-"use strict";
-var _a;
+var _a, _b, _c, _d;
+(_a = document.querySelector("#github")) === null || _a === void 0 ? void 0 : _a.addEventListener("click", () => window.open("https://github.com/DNAScanner/Schule/tree/main/K10/NwT/Projekt%201/Tests/Website"));
 const sizes = {
     rows: 10,
-    columns: 30,
+    columns: 40,
+};
+const selected = {
+    x: 0,
+    y: 0,
 };
 const randomCharacter = () => "dp"[Math.floor(Math.random() * 2)];
 for (let row = 0; row < sizes.rows; row++) {
     const rowElement = document.createElement("div");
     rowElement.classList.add("row");
-    (_a = document.querySelector("#row-wrapper")) === null || _a === void 0 ? void 0 : _a.appendChild(rowElement);
+    (_b = document.querySelector("#row-wrapper")) === null || _b === void 0 ? void 0 : _b.appendChild(rowElement);
     for (let column = 0; column < sizes.columns; column++) {
         const columnElement = document.createElement("div");
         columnElement.classList.add("column");
@@ -31,4 +35,86 @@ for (let row = 0; row < sizes.rows; row++) {
         columnElement.insertBefore(randomCharacterElement, columnElement.children[1]);
     }
 }
+const showText = (text) => {
+    if (text) {
+        document.querySelector("#text").textContent = text;
+        document.querySelector("#text").classList.remove("hidden");
+    }
+    else
+        document.querySelector("#text").classList.add("hidden");
+};
+const setHighlight = (x, y) => {
+    var _a;
+    selected.x = x;
+    selected.y = y;
+    for (const currentlyHighlighted of document.querySelectorAll(".highlight"))
+        currentlyHighlighted.classList.remove("highlight");
+    (_a = document.querySelector("#row-wrapper")) === null || _a === void 0 ? void 0 : _a.children[selected.y].children[selected.x].classList.add("highlight");
+};
+var Key;
+(function (Key) {
+    Key["MARK"] = "KeyZ";
+    Key["SKIP"] = "KeyN";
+})(Key || (Key = {}));
+const timeExceeded = (startTime, time) => Date.now() - startTime > time;
+const waitForKey = async (allowedKeys, exitCondition) => {
+    const key = {
+        pressed: false,
+        key: "",
+    };
+    const callback = (event) => {
+        if (!allowedKeys.includes(event.code))
+            return;
+        key.key = event.code;
+        key.pressed = true;
+    };
+    document.addEventListener("keydown", callback);
+    while (!key.pressed && !exitCondition())
+        await new Promise((resolve) => setTimeout(resolve, 5));
+    document.removeEventListener("keydown", callback);
+    return key.key;
+};
+showText("Wenn du bereit bist, drücke die Leertaste oder Enter");
+await waitForKey(["Space", "Enter"], () => false);
+showText("");
+const TIME_PER_ROW = 10000;
+for (let row = 0; row < sizes.rows; row++) {
+    const startTime = Date.now();
+    for (let column = 0; column < sizes.columns; column++) {
+        const columnElement = (_c = document.querySelector("#row-wrapper")) === null || _c === void 0 ? void 0 : _c.children[row].children[column];
+        setHighlight(column, row);
+        const key = await waitForKey([Key.MARK, Key.SKIP], () => timeExceeded(startTime, TIME_PER_ROW));
+        if (key === Key.MARK)
+            columnElement === null || columnElement === void 0 ? void 0 : columnElement.classList.add("marked");
+        // If this is the last column and the time has not been exceeded, show the text
+        if (column === sizes.columns - 1 && !timeExceeded(startTime, TIME_PER_ROW)) {
+            showText("Du warst sehr schnell, bitte warte kurz");
+            await new Promise((resolve) => setTimeout(resolve, startTime + TIME_PER_ROW - Date.now()));
+            showText("");
+            break;
+        }
+        if (timeExceeded(startTime, TIME_PER_ROW)) {
+            showText("Stopp! Du kommst gleich in die nächste Zeile");
+            await new Promise((resolve) => setTimeout(resolve, 2000));
+            showText("");
+            break;
+        }
+    }
+}
+showText("Das war die letzte Zeile, danke fürs Testen");
+await new Promise((resolve) => setTimeout(resolve, 1500));
+let score = 0;
+let achievable = 0;
+for (const row of document.querySelectorAll(".row"))
+    for (const column of row.children) {
+        const dots = column.querySelectorAll(".dot").length;
+        const letter = (_d = column.querySelector(".random-character")) === null || _d === void 0 ? void 0 : _d.textContent;
+        const marked = column.classList.contains("marked");
+        if (marked && dots === 2 && letter === "d")
+            score++;
+        if (dots === 2 && letter === "d")
+            achievable++;
+    }
+showText(`Dein Score: ${score / achievable * 100} von 100`);
+export {};
 //# sourceMappingURL=index.js.map
--- K10/NwT/Projekt 1/Tests/Website/index.ts
+++ K10/NwT/Projekt 1/Tests/Website/index.ts
@@ -1,6 +1,15 @@
+export {};
+
+document.querySelector("#github")?.addEventListener("click", () => window.open("https://github.com/DNAScanner/Schule/tree/main/K10/NwT/Projekt%201/Tests/Website"));
+
 const sizes = {
 	rows: 10,
-	columns: 30,
+	columns: 40,
+};
+
+const selected = {
+	x: 0,
+	y: 0,
 };
 
 type RandomCharacter = "d" | "p";
@@ -37,3 +46,101 @@ for (let row = 0; row < sizes.rows; row++) {
 		columnElement.insertBefore(randomCharacterElement, columnElement.children[1]);
 	}
 }
+
+const showText = (text: string): void => {
+	if (text) {
+		document.querySelector("#text")!.textContent = text;
+		document.querySelector("#text")!.classList.remove("hidden");
+	} else document.querySelector("#text")!.classList.add("hidden");
+};
+
+const setHighlight = (x: number, y: number): void => {
+	selected.x = x;
+	selected.y = y;
+
+	for (const currentlyHighlighted of document.querySelectorAll(".highlight")) currentlyHighlighted.classList.remove("highlight");
+
+	document.querySelector("#row-wrapper")?.children[selected.y].children[selected.x].classList.add("highlight");
+};
+
+enum Key {
+	MARK = "KeyZ",
+	SKIP = "KeyN",
+}
+
+const timeExceeded = (startTime: number, time: number) => Date.now() - startTime > time;
+
+const waitForKey = async (allowedKeys: string[], exitCondition: () => boolean) => {
+	const key = {
+		pressed: false,
+		key: "",
+	};
+
+	const callback = (event: KeyboardEvent) => {
+		if (!allowedKeys.includes(event.code)) return;
+
+		key.key = event.code;
+		key.pressed = true;
+	};
+
+	document.addEventListener("keydown", callback);
+	while (!key.pressed && !exitCondition()) await new Promise((resolve) => setTimeout(resolve, 5));
+	document.removeEventListener("keydown", callback);
+
+	return key.key;
+};
+
+showText("Wenn du bereit bist, drücke die Leertaste oder Enter");
+await waitForKey(["Space", "Enter"], () => false);
+showText("");
+
+const TIME_PER_ROW = 10000;
+
+for (let row = 0; row < sizes.rows; row++) {
+	const startTime = Date.now();
+
+	for (let column = 0; column < sizes.columns; column++) {
+		const columnElement = document.querySelector("#row-wrapper")?.children[row].children[column];
+
+		setHighlight(column, row);
+
+		const key = await waitForKey([Key.MARK, Key.SKIP], () => timeExceeded(startTime, TIME_PER_ROW));
+
+		if (key === Key.MARK) columnElement?.classList.add("marked");
+
+		// If this is the last column and the time has not been exceeded, show the text
+		if (column === sizes.columns - 1 && !timeExceeded(startTime, TIME_PER_ROW)) {
+			showText("Du warst sehr schnell, bitte warte kurz");
+			await new Promise((resolve) => setTimeout(resolve, startTime + TIME_PER_ROW - Date.now()));
+			showText("");
+
+			break;
+		}
+
+		if (timeExceeded(startTime, TIME_PER_ROW)) {
+			showText("Stopp! Du kommst gleich in die nächste Zeile");
+			await new Promise((resolve) => setTimeout(resolve, 2000));
+			showText("");
+
+			break;
+		}
+	}
+}
+
+showText("Das war die letzte Zeile, danke fürs Testen");
+await new Promise((resolve) => setTimeout(resolve, 1500));
+
+let score = 0;
+let achievable = 0;
+
+for (const row of document.querySelectorAll(".row"))
+	for (const column of row.children) {
+		const dots = column.querySelectorAll(".dot").length;
+		const letter = column.querySelector(".random-character")?.textContent;
+		const marked = column.classList.contains("marked");
+
+		if (marked && dots === 2 && letter === "d") score++;
+		if (dots === 2 && letter === "d") achievable++;
+	}
+
+showText(`Dein Score: ${score/achievable * 100} von 100`);
